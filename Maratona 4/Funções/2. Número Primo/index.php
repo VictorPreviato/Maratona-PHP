e97b1be78aee87e99fe8fce1d92d52cf0d40ddf7
Victor Previato <146777340+VictorPreviato@users.noreply.github.com>
2025-02-12 11:59:33 -0300

    Maratona 4

---
+++ Maratona 4/Funções/2. Número Primo/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+/*Crie uma função que receba um número e determine se ele é primo ou não.*/
+
+
+
+Function primo ($num) {
+    if ($num > 1 && $num % 1 == 0 && $num % $num == 0 )
+{
+        echo "O número $num é primo!"; 
+    } else {
+        echo "O número $num não é primo!"; }}
+
+
+
+
+
+$núme = range(1,7,1);
+
+function ($n){
+foreach {}
+
+
+}
+
+
+primo(-4);
+
+?>   
+
+</body>
+
+</html>
---
+++ Maratona 4/Laço for/2. Média Inteiros/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Media de Inteiros</title>
+</head>
+<body>
+    <h2>Media de Inteiros</h2>
+<?php 
+    /*Crie um programa que receba 10 números inteiros e calcule a média desses números. Utilize o laço for para ler os números e calcular a média */
+
+    for ($i = 0; $i > 10; $i++) {
+        $num = [10,20,30,40,50,15,26,89,100,90];
+        $int = count($num);//Conta quantos inteiros tem.
+        $media += $num;
+        $media /= $int; 
+    } 
+    echo "o valor da media é : $media /n" ;
+        
+    
+
+
+?>  
+
+
+    
+</body>
+</html>
